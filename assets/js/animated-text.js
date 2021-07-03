0eecd9c3a747147e18655a2dae5db1cae3e6c18c
Buddhi Rangana <82770183+buddhirangana@users.noreply.github.com>
2021-07-03 17:13:03 +0530

    Add files via upload

---
+++ assets/js/animated-text.js
@@ -0,0 +1,8 @@
+var fifteenth = 'Color Picker'; var sixteenth = '#ffffff'; var seventeenth = '#ffff66'; var eighteenth = '30'; var nineteenth = 150; var twentieth = 0; var first2;
+document.write('<div id=second2></div>');
+function sixth2() {
+    fifth2 = '<font size=' + eighteenth + '><font color=' + sixteenth + '>';
+    for (var first2 = 0; first2 < fifteenth.length; first2++) {
+        if (first2 == twentieth) { fifth2 += '<font color=' + seventeenth + '>' + fifteenth.charAt(twentieth) + '</font>'; } else { fifth2 += fifteenth.charAt(first2); }
+    } fifth2 += '</font></font>'; document.getElementById("second2").innerHTML = fifth2; (twentieth == fifteenth.length) ? twentieth = 0 : twentieth++;
+} setInterval('sixth2()', nineteenth);
